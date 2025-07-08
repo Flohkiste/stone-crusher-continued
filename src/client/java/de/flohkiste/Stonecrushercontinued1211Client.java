6d6895a43c5061f8fa43be400742336f72c840a7
Florian Kreher <flo@kreher-rv.de>
2025-07-08 19:27:36 +0200

    Copied Paper Template

---
+++ src/client/java/de/flohkiste/Stonecrushercontinued1211Client.java
@@ -0,0 +1,10 @@
+package de.flohkiste;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Stonecrushercontinued1211Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
